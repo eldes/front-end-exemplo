4c6a1ddcf6cd10cc745024a10a2e05e3467f6b5e
SENACEDU\eldes.poliveira <eldes.poliveira@sp.senac.br>
2022-02-23 22:11:33 -0300

    Implementa a estrutura da página Categorias

--- src/pages/Categorias.tsx
+++ src/pages/Categorias.tsx
@@ -4,6 +4,11 @@ import Pagina from "../components/Pagina";
 const CategoriasPage: FunctionComponent = () => {
 	return (
 		<Pagina titulo="Categorias">
+			<ul>
+				<li>Moveis</li>
+				<li>Eletrônicos</li>
+				<li>Publicações</li>
+			</ul>
 		</Pagina>
 	)
 }
