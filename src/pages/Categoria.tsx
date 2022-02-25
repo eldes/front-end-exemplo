a069b7c1a84807109c05537d761bb3c2be43c360
SENACEDU\eldes.poliveira <eldes.poliveira@sp.senac.br>
2022-02-24 21:20:24 -0300

    Remove listagem de subcategorias

--- src/pages/Categoria.tsx
+++ src/pages/Categoria.tsx
@@ -1,5 +1,5 @@
-import { FunctionComponent, useState } from "react"
-import { NavLink, useParams } from "react-router-dom"
+import { FunctionComponent, MouseEventHandler, useState } from "react"
+import { useParams } from "react-router-dom"
 import Pagina from '../components/Pagina'
 import categoriasService from '../services/categorias'
 
@@ -7,18 +7,16 @@ const CategoriaPage: FunctionComponent = () => {
 
 	const { id } = useParams()
 	const [categoria, setCategoria] = useState(categoriasService.ler(Number.parseInt(id ?? '')))
-	const [subCategorias, setSubCategorias] = useState(categoriasService.lerTodasSubcategorias(Number.parseInt(id ?? '')))
 
 	return (
 		<Pagina titulo={categoria?.nome ?? ''}>
 			<ul>
-			{
-				subCategorias.map( categoria =>
-					<li>
-						<NavLink to={`/categorias/${categoria.id}`}>{ categoria.nome }</NavLink>
-					</li>
-				)
-			}
+				<li>
+					Produto 1
+				</li>
+				<li>
+					Produto 2
+				</li>
 			</ul>
 		</Pagina>
 	)
