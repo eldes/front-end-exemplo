7ac2827ef3eb384a245db01bc4573036211b8123
Eldes <github@eldes.com>
2022-03-14 20:03:50 -0300

    [!] Corrige Página Categoria

--- src/pages/Categoria.tsx
+++ src/pages/Categoria.tsx
@@ -1,12 +1,20 @@
-import { FunctionComponent, MouseEventHandler, useState } from "react"
+import { FunctionComponent, MouseEventHandler, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Pagina from '../components/Pagina'
+import Categoria from "../models/categoria"
 import categoriasService from '../services/categorias'
+import Produto from '../models/produto'
 
 const CategoriaPage: FunctionComponent = () => {
 
 	const { id } = useParams()
-	const [categoria, setCategoria] = useState(categoriasService.ler(Number.parseInt(id ?? '')))
+	const [categoria, setCategoria] = useState<Categoria | null>(null)
+	const [produtos, setProdutos] = useState<Produto[]>()
+	
+	useEffect(() => {
+		categoriasService.ler(Number.parseInt(id ?? ''), categoria => setCategoria(categoria))
+		categoriasService.lerProdutos(Number.parseInt(id ?? ''), produtos => setProdutos(produtos))
+	})
 
 	return (
 		<Pagina titulo={categoria?.nome ?? ''}>
